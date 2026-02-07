06beb67d05b4ad69d7e5db19c6049a9ae22a6beb
Guilherme <guilhermeagenor1012@gmail.com>
2026-02-07 08:07:25 -0300

    feature: add cookie in middleware

--- src/http/middleware/ensure-Authenticated.ts
+++ src/http/middleware/ensure-Authenticated.ts
@@ -4,7 +4,7 @@ import { IJWTService } from "../../core/cryptography/IJwtService";
 export function ensureAuthenticated(jwtService: IJWTService) {
   return (req: Request, res: Response, next: NextFunction) => {
     // variable receive header
-    const authHeader = req.headers.authorization;
+    const authHeader = req.cookies?.access_token;
 
     if (!authHeader) {
       return res.status(401).json({ message: "Unauthorized" });
