8ecd37edd47b62f0fd41f64aba01f43e0eb7dba7
Guilherme <guilhermeagenor1012@gmail.com>
2026-02-07 15:54:15 -0300

    fix: port front end

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3333", // url front end
     credentials: true, //cookie
   }),
 );
